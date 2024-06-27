4ba1f8bae79e2a1844617ed584213a2ebf4b2453
Frank Tsai <franktsai@MEC-256-macbook-2.local>
2024-06-27 15:14:40 +0800

    Time: 38ms (76.79%), Space: 12.54MB (38.40%)

---
+++ 1-two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hash_map = {}
+        for idx, num in enumerate(nums):
+            complement = target - num
+            if complement in hash_map:
+                return [hash_map[complement], idx]
+            hash_map[num] = idx
