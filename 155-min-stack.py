fd4334a13a0e7fcaf6eb16017c752a19e4946bbf
Frank Tsai <monru830127@gmail.com>
2024-08-02 18:30:05 +0800

    Time: 52ms (72.69%), Space: 20.51MB (36.94%)

---
+++ 155-min-stack.py
@@ -0,0 +1,18 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+        self.min_stack.append(x if not self.min_stack else min(x, self.min_stack[-1]))
+
+    def pop(self):
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.min_stack[-1]
